64bc9e1f809df2d2614bf4647780ee3e612fc2ce
ShinikumiAlex <144524709+ShinikumiAlex@users.noreply.github.com>
2024-12-25 08:56:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inf25._12._2024___2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double price = 20.4;
+            for (int i = 2; i <= 20; i++)
+            {
+                Console.WriteLine($"{i} шт. = {price*i} руб.")
+            }
+            Console.ReadKey();
+        }
+    }
+}
