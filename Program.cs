7aeb117058f7f1953491540669bb3a15ede3cca6
ShinikumiAlex <144524709+ShinikumiAlex@users.noreply.github.com>
2024-12-25 08:56:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inf25._12._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.Write("20 ");
+            //}
+            //Console.ReadKey();
+
+            
+        }
+
+    }
+}
+
