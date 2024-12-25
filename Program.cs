090f1776c288e0dcdd05ed63ffb00de3d7c6e016
ShinikumiAlex <144524709+ShinikumiAlex@users.noreply.github.com>
2024-12-25 09:27:24 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inf25._12._2024___8
+{
+    class Program
+    {
+        static void Main()
+        {
+            PositiveNumbers(10, -20, 20);
+        }
+
+        static void PositiveNumbers(int count, int min, int max)
+        {
+            Random random = new Random();
+
+            Console.WriteLine("Положительные числа и их характеристики:");
+
+            for (int i = 0; i < count; i++)
+            {
+                int number = random.Next(min, max + 1);
+
+                if (number > 0)
+                {
+                    string parity = number % 2 == 0 ? "Четное" : "Нечетное";
+                    Console.WriteLine($"{number} — {parity}");
+                }
+            }
+        Console.ReadKey();
+        }
+    }
+}
